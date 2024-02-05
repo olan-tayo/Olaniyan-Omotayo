41b8a074a8bc5c8a994692e19392ebc020508a42
olan-tayo <olantayo3627@gmail.com>
2024-02-05 15:20:34 +0100

    feat: add animation to sidebar links and content block

--- src/blocks/Content.tsx
+++ src/blocks/Content.tsx
@@ -4,40 +4,33 @@ import React, { useEffect } from "react";
 import Home from "./Home/Home";
 import Projects from "./Projects/Projects";
 import Experience from "./Experience/Experience";
-import {
-  Link,
-  Button,
-  Element,
-  Events,
-  animateScroll as scroll,
-  scrollSpy,
-} from "react-scroll";
+import { Element } from "react-scroll";
 
 const Content = () => {
-  const scrollToTop = () => {
-    scroll.scrollToTop();
-  };
+  // const scrollToTop = () => {
+  //   scroll.scrollToTop();
+  // };
 
-  useEffect(() => {
-    // Registering the 'begin' event and logging it to the console when triggered.
-    Events.scrollEvent.register("begin", (to, element) => {
-      console.log("begin", to, element);
-    });
+  // useEffect(() => {
+  //   // Registering the 'begin' event and logging it to the console when triggered.
+  //   Events.scrollEvent.register("begin", (to, element) => {
+  //     console.log("begin", to, element);
+  //   });
 
-    // Registering the 'end' event and logging it to the console when triggered.
-    Events.scrollEvent.register("end", (to, element) => {
-      console.log("end", to, element);
-    });
+  //   // Registering the 'end' event and logging it to the console when triggered.
+  //   Events.scrollEvent.register("end", (to, element) => {
+  //     console.log("end", to, element);
+  //   });
 
-    // Updating scrollSpy when the component mounts.
-    scrollSpy.update();
+  //   // Updating scrollSpy when the component mounts.
+  //   scrollSpy.update();
 
-    // Returning a cleanup function to remove the registered events when the component unmounts.
-    return () => {
-      Events.scrollEvent.remove("begin");
-      Events.scrollEvent.remove("end");
-    };
-  }, []);
+  //   // Returning a cleanup function to remove the registered events when the component unmounts.
+  //   return () => {
+  //     Events.scrollEvent.remove("begin");
+  //     Events.scrollEvent.remove("end");
+  //   };
+  // }, []);
 
   return (
     <div className="w-full md:w-[55%] overflow-x-auto text-primary-50">
@@ -67,7 +60,6 @@ const Content = () => {
           <span className="underline underline-offset-2"> Tailwind CSS</span>,
           deployed with Vercel. All text is set in the Inter typeface.
         </p>
-        <p onClick={scrollToTop}>Scroll to top</p>
       </section>
     </div>
   );
