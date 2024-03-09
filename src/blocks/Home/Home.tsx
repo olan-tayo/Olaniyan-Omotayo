18860aeb2fac8acf2331f6d3d4df99301fd3d0a5
olan-tayo <olantayo3627@gmail.com>
2024-03-09 20:11:14 +0100

    chore: edit porfolio content

--- src/blocks/Home/Home.tsx
+++ src/blocks/Home/Home.tsx
@@ -6,10 +6,10 @@ const Home = () => {
           ABOUT
         </p>
         <p className="text-[15px] font-normal text-primary-50 leading-6 mb-6 md:mb-0">
-          Hello, I am Omotayo but people call me "Omotee". I am passionate abou
-          solving problems faced by the society using the best technologies. I
-          love paying attention to details. One of the things that makes me
-          excited is seeing ideas come to life.
+          Hello, I am Omotayo but people call me {'"'}Omotee{'"'}. I am
+          passionate abou solving problems faced by the society using the best
+          technologies. I love paying attention to details. One of the things
+          that makes me excited is seeing ideas come to life.
           <br />
           <br />
           When I{"’"}m not writing code, I{"’"}m usually going to the gym,
